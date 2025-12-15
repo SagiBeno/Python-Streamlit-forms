37900817f1a6e49c560b7ed5fd9e477410b6fb90
SagiBeno <benosagi321@gmail.com>
2025-12-15 08:05:51 +0100

    Handle POST

--- react-app/api/lies.js
+++ react-app/api/lies.js
@@ -1,7 +1,13 @@
 /** POST /api/lies */
 export default async function handler(req, res) {
-    const { method, body } = req;
-    console.log('Body: ', body);
-    // TODO - Process form data
-    return res.sendStatus(201);
+    const method = req.method;
+
+    if (method == 'POST') {
+        const body = req?.body;
+        // TODO - Process form data
+        console.log('Body: ', body);
+        return res.sendStatus(201);
+    } else {
+        return res.sendStatus(405);
+    }
 }
